b78db1fdbeb4f9bf202e41092c1a9c8cd249be00
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2023-11-06 19:04:26 +0300

    basic math functions definition

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -52,8 +52,25 @@ function evaluate({currentOperand,previousOperand,operation}){
   const prev= parseFloat(previousOperand)
   const current = parseFloat(currentOperand)
   if(isNaN(prev)|| isNaN(current)){
-    reducer
+    return ""
   }
+  let computation =""
+  switch(operation){
+    case "+":
+      computation = prev + current
+      break
+    case "-":
+      computation = prev - current
+      break
+    case "*":
+      computation = prev * current
+      break
+    case "/":
+      computation = prev / current
+      break
+  }
+  return computation.toString()
+
 }
 function App() {
   const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer,{})
