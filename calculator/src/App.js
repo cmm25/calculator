46dbfeac3bc27563ab0cb779d3e4c25b19156967
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2023-11-06 11:28:17 +0300

    calculator styling

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,4 +1,4 @@
-import './styles.css';
+import "./styles.css";
 
 function App() {
   return (
@@ -6,28 +6,25 @@ function App() {
       <div className="result-output">
         <div className ="previous-typed-input"></div>
         <div className ="current-typed-input"></div>
-        <button className="span-two">AC</button>
-        <button >DEL</button>
-        <button>/</button>
-        <button >1</button>
-        <button >2</button>
-        <button >3</button>
-        <button >*</button>
-        <button >4</button>
-        <button >5</button>
-        <button >6</button>
-        <button >+</button>
-        <button >7</button>
-        <button >8</button>
-        <button >9</button>
-        <button >-</button>
-        <button >.</button>
-        <button >0</button>
-        <button className="span-two"></button>
-
-        
       </div>
-      
+      <button className="span-two">AC</button>
+      <button >DEL</button>
+      <button>/</button>
+      <button >1</button>
+      <button >2</button>
+      <button >3</button>
+      <button >*</button>
+      <button >4</button>
+      <button >5</button>
+      <button >6</button>
+      <button >+</button>
+      <button >7</button>
+      <button >8</button>
+      <button >9</button>
+      <button >-</button>
+      <button >.</button>
+      <button >0</button>
+      <button className="span-two">=</button>      
     </div>
   );
 }
